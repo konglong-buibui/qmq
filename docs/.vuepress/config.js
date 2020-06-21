8dca335df35d4cefc46721452b016763c7a736ad
Your Name <you@example.com>
2020-06-21 23:57:47 +0800

    qmq

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,81 @@
+module.exports = {
+  "title": "小生狂谈",
+  "description": "我本将心向明月，奈何明月照沟渠",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/ico.png"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitLab",
+            "link": "https://gitlab.com/bobyuan/",
+            "icon": "reco-gitlab"
+          }
+        ]
+      }
+    ],
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "笑谈古人",
+        "desc": "不畏浮云遮望眼",
+      },
+      {
+        "title": "今朝后浪",
+        "desc": "我辈岂是蓬蒿人",
+        "email": "2662265320@qq.com",
+      }
+    ],
+	"logo": "/logo_trans.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "sidebar": "auto",
+    "lastUpdated": "Last Updated",
+    "author": "邱",
+    "authorAvatar": "/avatar.png",
+    //"record": "xxxx",
+    "startYear": "2020"
+  },
+  "markdown": {
+    "lineNumbers": false
+  }
+}
